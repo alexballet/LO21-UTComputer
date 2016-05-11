ad97e16d3e95e7b1bf2c7f00e28a1e913c879d93
Anton <aippolit@etu.utc.fr>
2016-05-11 14:39:03 +0200

    small changes (soyons rigoureux lel)

--- pile.cpp
+++ pile.cpp
@@ -31,17 +31,17 @@ QStack<Litteral*>* Pile::getStack() {
 
 void Pile::push(const QString& value, const QString& type){
     if(type=="Entier"){
-            bool ok;
-            Entier* entier = Entier::createLit(value.toInt(&ok, 10));
-            getStack()->push(entier);
-            emit modificationEtat();
-            return;
+        bool ok;
+        Entier* entier = Entier::createLit(value.toInt(&ok, 10));
+        getStack()->push(entier);
+        emit modificationEtat();
+        return;
     }
     if(type=="Reel"){
         QStringList parts = value.split('.', QString::KeepEmptyParts);
-        int e=0;
-        if(parts.at(0)!="")
-            e=parts.at(0).toInt();
+        int e = 0;
+        if(parts.at(0) != "")
+            e = parts.at(0).toInt();
         double d = e + parts.at(1).toInt()*(qPow(10,-(parts.at(1).length())));
         Reel* reel = Reel::createLit(d);
         getStack()->push(reel);
--- qcomputer.cpp
+++ qcomputer.cpp
@@ -7,47 +7,52 @@
 QComputer::QComputer(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::QComputer)
-    {
-        Pile* pile = Pile::getInstance();
-        QStack<Litteral*>* stack = pile->getStack();
+{
+    Pile* pile = Pile::getInstance();
+    QStack<Litteral*>* stack = pile->getStack();
 
-        ui->setupUi(this);
-        ui->vuePile->setRowCount(pile->getMaxAffiche());
-        ui->vuePile->setColumnCount(1);
-        ui->vuePile->verticalHeader()->setSectionResizeMode (QHeaderView::Fixed);
+    ui->setupUi(this);
+    ui->vuePile->setRowCount(pile->getMaxAffiche());
+    ui->vuePile->setColumnCount(1);
+    ui->vuePile->verticalHeader()->setSectionResizeMode (QHeaderView::Fixed);
 
-        QStringList numberList;
-        for(unsigned int i = pile->getMaxAffiche(); i>0; i--) {
-            QString str = QString::number(i);
-            str += " :";
-            numberList << str;
-            // creation of the item of each line initialized with an empty chain (chaine vide).
-            ui->vuePile->setItem(i-1, 0, new QTableWidgetItem(""));
-        }
-         ui->vuePile->setVerticalHeaderLabels(numberList);
-                ui->vuePile->setFixedHeight(pile->getMaxAffiche()*ui->vuePile->rowHeight(0)+2);
+    QStringList numberList;
+    for(unsigned int i = pile->getMaxAffiche(); i>0; i--) {
+        QString str = QString::number(i);
+        str += " :";
+        numberList << str;
+        // creation of the item of each line initialized with an empty chain (chaine vide).
+        ui->vuePile->setItem(i-1, 0, new QTableWidgetItem(""));
+    }
+    ui->vuePile->setVerticalHeaderLabels(numberList);
+    ui->vuePile->setFixedHeight(pile->getMaxAffiche() * ui->vuePile->rowHeight(0)+2);
 
-        // inhibit modification
-        ui->vuePile->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    // inhibit modification
+    ui->vuePile->setEditTriggers(QAbstractItemView::NoEditTriggers);
 
-        // connection
-        connect(pile,SIGNAL(modificationEtat()),this,SLOT(refresh()));
-        // first message
-        pile->setMessage("Bienvenue !");
-        // Give the command bar some focus.
-        ui->commande->setFocus(Qt::OtherFocusReason);
+    // connection
+    connect(pile,SIGNAL(modificationEtat()),this,SLOT(refresh()));
 
-        //connect all the keyboard buttons
-        QList<QPushButton*> buttons = this->findChildren<QPushButton*>();
-        foreach (QPushButton *button, buttons){
-            connect(button, SIGNAL(released()), this, SLOT(editCommmande()));
-        }
-        //disable keyboard
-        this->setFixedSize(589,322);
-        ui->clavier->hide();
-        ui->opLogiques->hide();
-        ui->opNumeriques->hide();
-        ui->opPile->hide();
+    // first message
+    pile->setMessage("Bienvenue !");
+    // set ui message
+    ui->message->setText(pile->getMessage());
+
+    // Give the command bar some focus.
+    ui->commande->setFocus(Qt::OtherFocusReason);
+
+    //connect all the keyboard buttons
+    QList<QPushButton*> buttons = this->findChildren<QPushButton*>();
+    foreach (QPushButton *button, buttons){
+        connect(button, SIGNAL(released()), this, SLOT(editCommmande()));
+    }
+
+    //disable keyboard
+    this->setFixedSize(589,322);
+    ui->clavier->hide();
+    ui->opLogiques->hide();
+    ui->opNumeriques->hide();
+    ui->opPile->hide();
 }
 
 QComputer::~QComputer()
@@ -59,10 +64,10 @@ void QComputer::refresh(){
     Pile* pile = Pile::getInstance();
     // the message
     ui->message->setText(pile->getMessage());
-    unsigned int nb=0;
+    unsigned int nb = 0;
     // delete everything
-    for(unsigned int i=0; i<pile->getMaxAffiche(); i++) ui->vuePile->item(i,0)->
-            setText("");
+    for(unsigned int i=0; i<pile->getMaxAffiche(); i++)
+        ui->vuePile->item(i,0)->setText("");
     // update
     QVectorIterator<Litteral*> it(*pile->getStack());
     for(it.toBack() ; it.hasPrevious() && nb<pile->getMaxAffiche(); nb++){
@@ -78,16 +83,17 @@ void QComputer::on_commande_returnPressed()
     // the actual message is not important anymore
     pile->setMessage("");
     // getting text for the command bar
-    QString c=ui->commande->text();
+    QString c = ui->commande->text();
     // extraction of each element from the line
     //(we suppose that <space> is the field separator)
     QTextStream stream(&c);
     QString com;
     do {
-        stream>>com; // element extraction
+        stream >> com; // element extraction
         // send the command to the controller
-        if (com!="") controleur->parse(com);
-    }while (com!="");
+        if (com != "")
+            controleur->parse(com);
+    }while (com != "");
     // empty the command line
     ui->commande->clear();
     emit pile->modificationEtat();
