d8007ca25415de02fdfb502f658dfe3a9e12dfe5
alexballet <brasilialex64@hotmail.fr>
2016-06-02 09:52:51 +0200

    atome.h doc

--- atome.h
+++ atome.h
@@ -12,16 +12,34 @@ private:
     QString id;
 public:
     /*!
-     * \brief Constructor of Atome class
-     * \param id - QString to initialize the id of the Atome object
+     * \brief Constructor of Atome class.
+     * \param id - QString to initialize the id of the Atome object.
      */
     Atome(QString id);
+    /*!
+     * \brief Accessor of the id attribute.
+     * \return Returns the id attribute of the Atome object.
+     */
     QString getId() const;
+    /*!
+     * \brief Displays the Atome object as a QString using its id attribute.
+     * \return Returns a QString composed of the id attribute.
+     *
+     * This method is used to display the Atome object in the QTableWidget of the mainWindow.
+     * The method creates a QString composed by the id put between two simple quotes.
+     * E.g. the Atome X becomes 'X'.
+     */
     QString toString() const;
+    /*!
+     * \brief Checks if the Atome is used as a Variable or a Programme.
+     * \return If the Atome is used as a Variable or a Programme, it returns it's value. Otherwise, it returns a new Expression.
+     *
+     * This method searches for the Atome id attribute in VariableMap and ProgrammeMap.
+     * If it finds the id in VariableMap, it returns the Variable.
+     * If it finds ths id in ProgrammeMap, it returns the Programme.
+     * If it doesn't find it, it returns a new Expression composed of the id attribute.
+     */
     Litteral* getLitterale() const;
 };
-
-template<class T>
-bool isAtome(T& a);
 
 #endif // ATOME_H
